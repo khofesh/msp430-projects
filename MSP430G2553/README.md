a72fe516e167488e2fe3421f54123b2691c5c7a8
fahmi ahmad <fahmiahmad@tutanota.com>
2025-11-26 18:01:44 +0700

    doesn't work

--- MSP430G2553/README.md
+++ MSP430G2553/README.md
@@ -5,3 +5,22 @@
 - sample code: https://e2e.ti.com/support/microcontrollers/msp-low-power-microcontrollers-group/msp430/f/msp-low-power-microcontroller-forum/963485/msp-exp430g2et-sample-code
   - https://dev.ti.com/tirex/explore/node?node=A__AJdFRf2UR1swxVvK3reQZg__msp430ware__IOGqZri__LATEST
   - https://dev.ti.com/tirex/explore/node?node=A__ACpzo3y2HDiShoWUbVgmbg__msp430ware__IOGqZri__LATEST
+
+right pins
+
+- DVSS
+- XIN/P2.6/TA0.1
+- XOUT/P2.7
+- TEST/SBWTCK
+- RST/NMI/SBWTDIO
+- P1.7/CAOUT/UCB0SIMO/UCB0SDA/A7/CA7/TDO/TDI
+- P1.6/TA0.1/UCB0SOMI/UCB0SCL/A6/CA6/TDI/TCLK
+- P3.7/TA1CLK/CAOUT
+
+left pins
+
+- DVCC
+- P1.0/TA0CLK/ACLK/A0/CA0
+- P1.1/TA0.0/UCA0RXD/UCA0SOMI/A1/CA1
+- P1.2/TA0.1/UCA0TXD/UCA0SIMO/A2/CA2
+- P1.3/ADC10CLK/CAOUT/VREF-/VEREF-/A3/CA3
---
+++ MSP430G2553/dht22_to_uart/main.c
@@ -0,0 +1,63 @@
+#include <msp430.h>
+#include "dht22.h"
+#include <stdio.h>
+
+char txbuf[256];
+
+void uart_init()
+{
+    UCA0CTL0 = 0;
+    UCA0CTL1 |= UCSWRST;           // Put USCI in reset mode
+    UCA0CTL1 = UCSSEL_2 | UCSWRST; // Use SMCLK, still reset
+    UCA0MCTL = UCBRF_0 | UCBRS_6;
+    UCA0BR0 = 131; // 9600 bauds
+    UCA0BR1 = 6;
+    UCA0CTL1 &= ~UCSWRST; // Normal mode
+    P1SEL = BIT1 + BIT2;  // P1.1 = RXD, P1.2=TXD
+    P1SEL2 = BIT1 + BIT2;
+}
+
+void uart_send(int len)
+{
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        while (UCA0STAT & UCBUSY)
+            ;
+        UCA0TXBUF = txbuf[i];
+    }
+}
+
+void init_clock_16mhz()
+{
+    if (CALBC1_16MHZ == 0xFF) // If calibration constant erased
+    {
+        while (1)
+            ;
+    }
+
+    DCOCTL = 0;
+    BCSCTL1 = CALBC1_16MHZ;
+    DCOCTL = CALDCO_16MHZ;
+}
+
+int main()
+{
+    WDTCTL = WDTPW | WDTHOLD;
+
+    init_clock_16mhz();
+
+    uart_init();
+
+    _BIS_SR(GIE);
+
+    while (1)
+    {
+        dht_start_read();
+        int t = dht_get_temp();
+        int h = dht_get_rh();
+        uart_send(sprintf(txbuf, "%3d.%1d C; %3d.%1d %%RH\r\n", t / 10, t % 10, h / 10, h % 10));
+    }
+
+    return 0;
+}
